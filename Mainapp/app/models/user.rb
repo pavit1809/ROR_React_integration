d8c3eaea4f39f28d5c7a3210f598691c72a864ae
pavit_1809 <pavitragoyal@hotmail.com>
2021-06-01 02:45:12 +0530

    Created and tested apis for task-2

--- Mainapp/app/models/user.rb
+++ Mainapp/app/models/user.rb
@@ -1,13 +1,25 @@
 class User < ApplicationRecord
   has_many :sips
   has_many :lumpsums
+
   VALID_PAN_REGEX=/[A-Z]{5}[0-9]{4}[A-Z]{1}/
-  validates :name,:email,:password,:phno,:state,:city,:dob,:pan, presence: true
-  validates :email, :pan, uniqueness: true
-  validates :name, length: {maximum: 50,minimum: 2}
-  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
-  validates :phno, length: {is: 10}
-  validates :pan, format:{with: VALID_PAN_REGEX}
+
+
+  #necessary conditions
+  validates :email,:password, presence: true
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP},uniqueness:true
+
+  #only needed if user is a paid one and not a free one
+  with_options if: :user_is_paid? do |user|
+    user.validates :name,:phno,:state,:city,:dob,:role,:pan, presence: true
+    user.validates :pan, format:{with: VALID_PAN_REGEX},uniqueness: true
+    user.validates :name, length: {maximum: 50,minimum: 2}
+    user.validates :phno, length: {is: 10}
+  end
+
+  def user_is_paid?
+    role == "user"
+  end
 
 end
 
