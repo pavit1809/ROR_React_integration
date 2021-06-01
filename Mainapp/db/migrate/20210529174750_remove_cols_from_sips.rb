b6fe78d680651c33c16df9ebb571c630bcd6facf
pavit_1809 <pavitragoyal@hotmail.com>
2021-06-01 21:39:33 +0530

    Reconfiguration p1

--- Mainapp/db/migrate/20210529174750_remove_cols_from_sips.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColsFromSips < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :sips, :totalInvestmentAmount, :float
-    remove_column :sips, :totalEstReturn, :float
-  end
-end
--- Mainapp/db/migrate/20210529180718_change_date_to_be_string_in_sips.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDateToBeStringInSips < ActiveRecord::Migration[6.1]
-  def change
-    change_column :sips, :dateOfApplication, :string
-    change_column :sips, :dateOfMaturity, :string
-  end
-end
