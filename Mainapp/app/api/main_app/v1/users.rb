53725a44ac3e5dff0155597c906c6ade398cbbd7
pavit_1809 <pavitragoyal@hotmail.com>
2021-06-02 19:35:20 +0530

    Refactored Code

--- Mainapp/app/api/main_app/v1/users.rb
+++ Mainapp/app/api/main_app/v1/users.rb
@@ -1,4 +1,4 @@
-require_relative "./genHelpers.rb"
+require_relative './Helpers/UserHelper'
 
 module MainApp
   module V1
@@ -8,111 +8,44 @@ module MainApp
       prefix :api
 
       resources :users do
-
         desc 'Create new User'
         params do
-          requires :pan,:dob,:city,:state,:phno,:name, :email,:password,:role
-          #string is default
+          requires :role, :email
+          requires :password, allow_blank: false
+          given role: ->(val) { val == 'user' } do
+            requires :pan, :dob, :city, :state, :phno, :name
+          end
         end
         post '/new' do
-          user_hash=Helper.generateUserHash(params)
-          user=User.new(user_hash)
-          if (user.valid?)
-            status 201
-            user.save
-            present({success: true})
-          else
-            status 400
-            present ({success: false,message: user.errors.messages})
-          end
+          present Helper.handle_user_signup(params)
+        end
+
+        desc 'Login User'
+        params do
+          requires :email, :password
+        end
+        post '/login' do
+          present Helper.handle_user_login(params)
         end
 
         desc 'Basic Details'
         params do
           requires :id, type: Integer
-          requires :token
+          requires :token, :role
         end
         get '/details' do
-          if (!Helper.checkTokenValidity?(params[:id],params[:token]))
-            status 401
-            present ({success: false})
-          else
-            user=User.find_by(id: params[:id])
-            present({name:user.name,email: user.email,pan:user.pan})
-          end
-        end
-
-        desc 'Visitor Entry'
-        params do
-          requires :email,:password,:role
-        end
-        post '/visitor' do
-          visitor=User.find_by(email: params[:email],role: params[:role])
-          if (visitor == nil)
-            #new visitor
-            visitor_hash=Helper.generateVisitorHash(params)
-            visitor=User.new(visitor_hash)
-            if (visitor.valid?)
-              visitor.save
-              #use login function here
-              ret=Helper.checkLoginCredentials(params[:email],params[:password])
-              if (ret == nil)
-                status 401
-                present ({success: false})
-              else
-                status 201
-                present({token: ret[:ret_token],id: ret[:ret_user].id},role: ret[:ret_user].role)
-              end
-            else
-              #ill-format request
-              status 400
-              present ({success: false,message: visitor.errors.messages})
-            end
-          else
-            #use login function
-            ret=Helper.checkLoginCredentials(params[:email],params[:password])
-            if (ret == nil)
-              status 401
-              present ({success: false})
-            else
-              status 200
-              present({token: ret[:ret_token],id: ret[:ret_user].id},role: ret[:ret_user].role)
-            end
-          end
-        end
-
-        desc 'Login User'
-        params do
-          requires :email,:password
-          #string is default
-        end
-        post '/login' do
-          ret=Helper.checkLoginCredentials(params[:email],params[:password])
-          if (ret==nil)
-            status 401
-            present ({success: false})
-          else
-            present({token: ret[:ret_token],id: ret[:ret_user].id},role: ret[:ret_user].role)
-          end
+          Helper.handle_user_details(params)
         end
 
         desc 'Logout User'
         params do
-          requires :id, type:Integer
-          requires :token, type:String
+          requires :id, type: Integer
+          requires :token, :role
         end
         post '/logout' do
-          if (!Helper.checkTokenValidity?(params[:id],params[:token]))
-            status 401
-            present ({success: false})
-          else
-            user=User.find_by(id: params[:id])
-            user.update_attribute(:token,nil)
-            present({success:true})
-          end
+          present Helper.handle_user_logout(params)
         end
-
-      end #-> end of resources
+      end
     end
   end
 end
